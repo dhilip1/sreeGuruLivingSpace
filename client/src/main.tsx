e51affa24af320efaedc8909f70fe687589d3b09
dhilip1 <33248340+dhilip1@users.noreply.github.com>
2025-04-19 19:48:04 +0000

    updated

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -8,9 +9,12 @@ createRoot(document.getElementById("root")!).render(
     <Helmet>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;600;700&family=Nunito+Sans:wght@300;400;600&family=Cinzel:wght@400;600&display=swap" rel="stylesheet" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;600;700&family=Nunito+Sans:wght@300;400;600&family=Cinzel:wght@400;600&display=swap"
+        rel="stylesheet"
+      />
       <title>Sree Guru Living Space Science</title>
     </Helmet>
     <App />
-  </HelmetProvider>
+  </HelmetProvider>,
 );
