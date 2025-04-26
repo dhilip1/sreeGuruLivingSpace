ebe50daf018038640206f3482dcbe6d0adf65869
dhilip1 <dhilip1@gmail.com>
2025-04-26 23:03:27 +0530

    updated for frontend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,22 +6,15 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
    <Helmet>
-  {/* Preconnect early */}
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" />
-
-  {/* Preload the font stylesheets */}
-  <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;600;700&family=Nunito+Sans:wght@300;400;600&family=Cinzel:wght@400;600&display=swap" />
-
-  {/* Load them properly */}
   <link
     href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;600;700&family=Nunito+Sans:wght@300;400;600&family=Cinzel:wght@400;600&display=swap"
     rel="stylesheet"
-    media="all"
   />
-
   <title>Sree Guru Living Space Science</title>
 </Helmet>
+
 
     <App />
   </HelmetProvider>,
