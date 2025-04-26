eadb5f94146827628befa58863001c5f3bc2b297
dhilip1 <dhilip1@gmail.com>
2025-04-26 23:30:07 +0530

    updated for frontend

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -4,7 +4,7 @@ import typographyPlugin from "@tailwindcss/typography";
 
 const config = {
   darkMode: ["class"],
-  content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       borderRadius: {
