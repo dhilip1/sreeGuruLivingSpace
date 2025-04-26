da23bad6e2de72c4945131620eb1f691a39b2ee0
dhilip1 <dhilip1@gmail.com>
2025-04-26 21:26:13 +0530

    updated for frontend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,41 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
-import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
-export default defineConfig(async () => {
-  return {
-    root:"./client",
-    plugins: [
-      react(),
-      runtimeErrorOverlay(),
-      themePlugin(),
-      ...(process.env.NODE_ENV !== "production" &&
-      process.env.REPL_ID !== undefined
-        ? [(await import("@replit/vite-plugin-cartographer")).cartographer()]
-        : []),
-    ],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "src"),
-        "@shared": path.resolve(__dirname, "..", "shared"),
-        "@assets": path.resolve(__dirname, "..", "attached_assets"),
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "https://sreegurulivingspace.onrender.com",
+        changeOrigin: true,
       },
     },
-    server: {
-      port: 5173,
-      hmr: true,
-      proxy: process.env.NODE_ENV === 'development'
-      ? {
-          '/api': {
-            target: 'http://localhost:8080',
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, ''),
-          },
-        }
-      : undefined,
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
     },
-    
-  };
+  },
 });
