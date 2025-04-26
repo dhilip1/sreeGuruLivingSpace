633265e26d5be3b6192bdc709d2562f6dc8d5ed2
dhilip1 <dhilip1@gmail.com>
2025-04-26 22:59:30 +0530

    updated for frontend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -6,15 +5,24 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
-    <Helmet>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;600;700&family=Nunito+Sans:wght@300;400;600&family=Cinzel:wght@400;600&display=swap"
-        rel="stylesheet"
-      />
-      <title>Sree Guru Living Space Science</title>
-    </Helmet>
+   <Helmet>
+  {/* Preconnect early */}
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+  {/* Preload the font stylesheets */}
+  <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;600;700&family=Nunito+Sans:wght@300;400;600&family=Cinzel:wght@400;600&display=swap" />
+
+  {/* Load them properly */}
+  <link
+    href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;600;700&family=Nunito+Sans:wght@300;400;600&family=Cinzel:wght@400;600&display=swap"
+    rel="stylesheet"
+    media="all"
+  />
+
+  <title>Sree Guru Living Space Science</title>
+</Helmet>
+
     <App />
   </HelmetProvider>,
 );
